ba5efc5b3d3092ab6773ffdf2eb9e5249f0d84aa
anawavees <ana.plaksik@gmail.com>
2021-09-24 18:09:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,49 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-class Lecturer(Mentor):
-    pass
-class Reviewer(Mentor):
-    pass
 
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def rate_hw_avg(self):
+        sum = 0
+        quantity = 0
+        for val in self.values():
+            for grade in val:
+                sum += grade
+                quantity += 1
+                average = sum / quantity
+        return average
+
+    def __str__(self):
+        res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.rate_hw_avg}'
+        return res
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+best_student = Student('Ruoy', 'Eman', 'Mickle')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
