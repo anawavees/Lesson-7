8e567b9d94d5c48027ba6b391c019b164144f435
anawavees <ana.plaksik@gmail.com>
2021-09-27 21:01:45 +0300

    2

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw_avg(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -20,20 +29,15 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
-    def rate_hw_avg(self):
+    def calc_avg(self):
         sum = 0
         quantity = 0
-        for val in self.values():
+        for val in self.grades.values():
             for grade in val:
                 sum += grade
                 quantity += 1
                 average = sum / quantity
         return average
-
-    def __str__(self):
-        res = f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.rate_hw_avg}'
-        return res
-
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -49,14 +53,24 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'Mickle')
-best_student.courses_in_progress += ['Python']
+best_student = Student('Ruoy', 'Eman', 'our_gender')
+best_student.courses_in_progress += ['Python', 'Введение в программирование']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 7)
+cool_mentor.rate_hw(best_student, 'Python', 9)
+
+cool_lector = Lecturer('Tony', 'Stark')
+cool_lector.courses_attached += ['Введение в программирование']
+
+best_student.rate_hw_avg(cool_lector, 'Введение в программирование', 8)
+best_student.rate_hw_avg(cool_lector, 'Введение в программирование', 2)
+
+print(cool_lector.grades)
+
+print(cool_lector.calc_avg())
 
 print(best_student.grades)
